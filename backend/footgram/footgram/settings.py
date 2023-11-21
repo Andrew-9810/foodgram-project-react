5c95b795808a9ab72580225d0f0fc91d6c4451c6
Andrew-9810 <andrew-9810@yandex.ru>
2023-11-21 08:06:05 +0000

    PEP8

--- backend/footgram/footgram/settings.py
+++ backend/footgram/footgram/settings.py
@@ -72,14 +72,14 @@ WSGI_APPLICATION = 'footgram.wsgi.application'
 
 
 DATABASES = {
-  'default': {
-    'ENGINE': 'django.db.backends.postgresql',
-    'NAME': os.getenv('POSTGRES_DB', 'django'),
-    'USER': os.getenv('POSTGRES_USER', 'django'),
-    'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
-    'HOST': os.getenv('DB_HOST', ''),
-    'PORT': os.getenv('DB_PORT', 5432)
-  }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', 'django'),
+        'USER': os.getenv('POSTGRES_USER', 'django'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+        'HOST': os.getenv('DB_HOST', ''),
+        'PORT': os.getenv('DB_PORT', 5432)
+    }
 }
 
 AUTH_PASSWORD_VALIDATORS = [
